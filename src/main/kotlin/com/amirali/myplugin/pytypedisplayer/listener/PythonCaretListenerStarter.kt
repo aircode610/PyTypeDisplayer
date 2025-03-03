15102ea91af75a50c0895b11c3c77295e819b852
Amirali Iranmanesh <34022796+aircode610@users.noreply.github.com>
2025-03-03 19:52:31 +0100

    Added a listener so the widget changes when the caret changes position

---
+++ src/main/kotlin/com/amirali/myplugin/pytypedisplayer/listener/PythonCaretListenerStarter.kt
@@ -0,0 +1,13 @@
+package com.amirali.myplugin.pytypedisplayer.listener
+
+import com.amirali.myplugin.pytypedisplayer.service.PythonCaretListener
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.ProjectActivity
+
+class PythonCaretListenerStarter : ProjectActivity {
+
+    override suspend fun execute(project: Project) {
+        // Initialize the caret listener
+        project.getService(PythonCaretListener::class.java)
+    }
+}
